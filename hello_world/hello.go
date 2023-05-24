f3cb78aab4e048036fd7739522e081258acc8ba5
Eli Fuchsman <efuchsman@gmail.com>
2023-05-24 16:48:11 -0600

    WIP

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -4,9 +4,16 @@ func Hello() string {
 	return "Hello, World"
 }
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func HelloWithName(name string) string {
 
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -2,6 +2,12 @@ package hello_world
 
 import "testing"
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
 func TestHello(t *testing.T) {
 	got := Hello()
 	want := "Hello, World"
@@ -12,10 +18,18 @@ func TestHello(t *testing.T) {
 }
 
 func TestHelloWithName(t *testing.T) {
-	got := HelloWithName("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := HelloWithName("Chris")
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("empty string defaults to world", func(t *testing.T) {
+		got := HelloWithName("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+
+	})
 }
