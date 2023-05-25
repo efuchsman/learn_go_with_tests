6c0b1a6f3b3d8190f9c5c422e8aac795e6c1f97a
Eli Fuchsman <efuchsman@gmail.com>
2023-05-25 13:52:37 -0600

    Wip

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,10 +1,28 @@
 package arrays_slices
 
-func Sum(numbers [5]int) int {
-	sum := 0
+// func Sum(numbers [5]int) int {
+// 	sum := 0
 
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+// 	for i := 0; i < 5; i++ {
+// 		sum += numbers[i]
+// 	}
+// 	return sum
+// }
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
