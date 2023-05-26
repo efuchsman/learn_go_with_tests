86e647520da9306db055f48f2f2b9197293a4b72
Eli Fuchsman <efuchsman@gmail.com>
2023-05-26 15:48:07 -0600

    Triangle area

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,14 +1,37 @@
 package structs_methods_interfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return (r.Height * r.Width)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * (c.Radius * c.Radius)
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
 }
